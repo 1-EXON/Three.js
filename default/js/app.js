7ca1c8d2785eecfe0ea6e27b4764e408abc278d0
시혁 박 <exon.indiv@gmail.com>
2022-05-28 10:14:31 +0900

    아주 멋진 상자

--- default/js/app.js
+++ default/js/app.js
@@ -1,5 +1,8 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
+
+// Scene (장면) 생성
 const scene = new THREE.Scene()
+// Camera 생성
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer()
@@ -8,28 +11,29 @@ renderer.setClearColor(0xffffff,1.0)
 document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
-const material1 = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
-const cube1 = new THREE.Mesh(geometry, material1)
-scene.add(cube1)
-const cube2 = new THREE.Mesh(geometry, material1)
-scene.add(cube2)
-const cube3 = new THREE.Mesh(geometry, material1)
-scene.add(cube3)
-const cube4 = new THREE.Mesh(geometry, material1)
-scene.add(cube4)
+const material1 = new THREE.MeshStandardMaterial({
+    color: 0xff8d75,
+    metalness: 0.2,
+    transparent: true,
+    opacity: 1
+})
 
-scene.overrideMaterial = new THREE.MeshLambertMaterial({ color: 0x44aa88 })
+const cubes = new Array()
+for (let i = 0; i < 5; i++) {
+    cubes.push(new THREE.Mesh(geometry, material1))
+    scene.add(cubes[i])
+}
+
+cubes[4].position.set(5, 0, 0)
+
 scene.fog = new THREE.Fog(0xffffff, 0.015, 100)
 
-
-
 const light = new THREE.PointLight()
-light.position.set(0, 0, 5)
+light.position.set(0, 10, 100)
 scene.add(light)
-// let helper = new THREE.CameraHelper(light.shadow.camera)
-// scene.add(helper)
 
-window.addEventListener( 'resize', onWindowResize );
+// 반응형
+window.addEventListener('resize', onWindowResize)
 
 camera.position.z = 10
 
@@ -37,24 +41,26 @@ camera.position.z = 10
 function animate() {
     requestAnimationFrame(animate)
 
-    cube1.rotateY(-0.01)
-    cube1.rotateX(0.01)
-    cube2.rotateY(0.01)
-    cube2.rotateX(-0.01)
-    cube3.rotateY(-0.01)
-    cube3.rotateX(-0.01)
-    cube4.rotateY(0.01)
-    cube4.rotateX(0.01)
+    cubes[0].rotateY(-0.01)
+    cubes[0].rotateX(0.01)
+    cubes[1].rotateY(0.01)
+    cubes[1].rotateX(-0.01)
+    cubes[2].rotateY(-0.01)
+    cubes[2].rotateX(-0.01)
+    cubes[3].rotateY(0.01)
+    cubes[3].rotateX(0.01)
+
+    camera.rotateZ(0.01)
 
     renderer.render(scene, camera)
 }
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight )
 
 }
 
