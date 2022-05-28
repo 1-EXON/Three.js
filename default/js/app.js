1916a40ff3650bef8c124858d4baab09491bf2d5
시혁 박 <exon.indiv@gmail.com>
2022-05-28 09:52:33 +0900

    멋진 상자

--- default/js/app.js
+++ default/js/app.js
@@ -1,39 +1,62 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0xffffff,1.0)
 document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
-const material1 = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const material1 = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
 const cube1 = new THREE.Mesh(geometry, material1)
-scene.add(cube1);
+scene.add(cube1)
+const cube2 = new THREE.Mesh(geometry, material1)
+scene.add(cube2)
+const cube3 = new THREE.Mesh(geometry, material1)
+scene.add(cube3)
+const cube4 = new THREE.Mesh(geometry, material1)
+scene.add(cube4)
+
+scene.overrideMaterial = new THREE.MeshLambertMaterial({ color: 0x44aa88 })
+scene.fog = new THREE.Fog(0xffffff, 0.015, 100)
+
+
+
+const light = new THREE.PointLight()
+light.position.set(0, 0, 5)
+scene.add(light)
+// let helper = new THREE.CameraHelper(light.shadow.camera)
+// scene.add(helper)
+
+window.addEventListener( 'resize', onWindowResize );
 
 camera.position.z = 10
-
-
-window.addEventListener("keydown", (e) => {
-    if (e.key === 'w') cube1.translateY(0.1)
-    else if (e.key === 'a') cube1.translateX(-0.1)
-    else if (e.key === 's') cube1.translateY(-0.1)
-    else if (e.key === 'd') cube1.translateX(0.1)
-    else if (e.key === 'ArrowUp') cube1.rotateY(0.1)
-    else if (e.key === 'ArrowLeft') cube1.rotateX(-0.1)
-    else if (e.key === 'ArrowDown') cube1.rotateY(-0.1)
-    else if (e.key === 'ArrowRight') cube1.rotateX(0.1)
-    
-})
 
 
 function animate() {
     requestAnimationFrame(animate)
 
+    cube1.rotateY(-0.01)
+    cube1.rotateX(0.01)
+    cube2.rotateY(0.01)
+    cube2.rotateX(-0.01)
+    cube3.rotateY(-0.01)
+    cube3.rotateX(-0.01)
+    cube4.rotateY(0.01)
+    cube4.rotateX(0.01)
+
     renderer.render(scene, camera)
 }
 
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 
 animate()
